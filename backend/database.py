0611a64c54207f9644328e33449e1b9c3220ffe5
swaroopCh72 <221663459+swaroopCh72@users.noreply.github.com>
2025-12-26 20:35:50 +0530

    Dockerize backend, frontend

--- backend/database.py
+++ backend/database.py
@@ -1,5 +1,20 @@
+
+import os
 from pymongo import MongoClient
 
-client = MongoClient("mongodb://localhost:27017")
+MONGO_USERNAME = os.getenv("MONGO_USERNAME")
+MONGO_PASSWORD = os.getenv("MONGO_PASSWORD")
+MONGO_HOST = os.getenv("MONGO_HOST")
+MONGO_PORT = os.getenv("MONGO_PORT", "27017")
+
+if not all([MONGO_USERNAME, MONGO_PASSWORD, MONGO_HOST]):
+    raise RuntimeError("MongoDB environment variables are not set correctly")
+
+MONGO_URI = (
+    f"mongodb://{MONGO_USERNAME}:{MONGO_PASSWORD}"
+    f"@{MONGO_HOST}:{MONGO_PORT}/admin"
+)
+
+client = MongoClient(MONGO_URI)
 db = client["login_app"]
 users_collection = db["users"]
